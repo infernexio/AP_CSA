edbe856fdce23819078690e1c0bd0bc03a71ceca
sohailshaik <sohail382517@gmail.com>
2020-02-06 22:50:45 -0500

    finished game of life !!!!!!!!!!!!

--- GameOfLife/src/SSDriver.java
+++ GameOfLife/src/SSDriver.java
@@ -6,43 +6,44 @@ import java.util.Scanner;
  * @Assignment - SSDriver
  */
 public class SSDriver {
-	
+
+	public static int day;
+
 	public static void main(String args[]) {
 		Scanner Stringsc = new Scanner(System.in);
 		Scanner sc = new Scanner(System.in);
 		SSGameOfLife game = new SSGameOfLife(errorCheck("Enter a integer for the number of rows: ", sc), errorCheck("Enter a integer for the number of cols: ", sc));
 		String userInput = "";
-		
+		day = 0;
+
 		do {
-		game.initilaize(errorCheck("Enter row of live cell: ", sc, game.getArr().length), errorCheck("Enter col for live cell: ", sc, game.getArr().length));
+			game.initilaize(errorCheck("Enter row of live cell: ", sc, game.getArr().length), errorCheck("Enter col for live cell: ", sc, game.getArr()[0].length));
 		}while(errorCheckString("Do you want to add more live cells", Stringsc).equalsIgnoreCase("y"));
-		
+
 		do {
-			for(int row = 0; row < game.getArr().length; row++) {
-				for(int col = 0; col  <game.getArr()[row].length; col++) {
-					if(userInput.equalsIgnoreCase("y")){
-						if(game.isAlive(row, col)) {
-							if(game.getNeighbors(row, col) > 2) {
-								game.getArr()[row][col] = SSGameOfLife.ALIVE_CELL;
-							}else {
-								game.getArr()[row][col] = SSGameOfLife.DEAD_CELL;
-							}
-						}else if(!game.isAlive(row, col)) {
-							if(game.getNeighbors(row, col) == 3) {
-								game.getArr()[row][col] = SSGameOfLife.ALIVE_CELL;
-							}else {
-								game.getArr()[row][col] = SSGameOfLife.DEAD_CELL;
-							}
-						}
+			System.out.println("Day: " + day);
+
+			if(day > 0){
+				game.playGame();
+				for(int i = 0; i < game.getArr().length; i++) {
+					for(int j = 0; j  < game.getArr()[i].length; j++) {
+						System.out.print(game.getArr()[i][j]);
 					}
-					System.out.print(game.getArr()[row][col]);
+					System.out.println();
 				}
-				System.out.println();
+			}else {
+				for(int row = 0; row < game.getArr().length; row++) {
+					for(int col = 0; col  <game.getArr()[row].length; col++) {
+						System.out.print(game.getArr()[row][col]);
+					}
+					System.out.println();
+				}
 			}
-			userInput = errorCheckString("Do you want to see the next day (y/n)", Stringsc);
-		}while(userInput.equalsIgnoreCase("y"));
+			day++;
+		}while(game.getAmountAlive() > 0 && (errorCheckString("Do you want to see the next day (y/n)", Stringsc)).equalsIgnoreCase("y"));
+		System.out.println("Everyone Died!");
 	}
-	
+
 	/**
 	 * checks for wrong input to a promt
 	 * @param promt - the promt the program wants to ask the user
@@ -86,7 +87,7 @@ public class SSDriver {
 		}while(userNum < 0);
 		return userNum;
 	}
-	
+
 	/**
 	 * checks for wrong input to a promt
 	 * @param promt - the promt the program wants to ask the user
--- GameOfLife/src/SSGameOfLife.java
+++ GameOfLife/src/SSGameOfLife.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
  * @Assignment - SSGameOfLife
  */
 public class SSGameOfLife {
-	private boolean isAlive;
 	private String[][] arr;
-	
+	private String[][] nums;
+
 	public static final String DEAD_CELL = "0";
 	public static final String ALIVE_CELL = "X";
-	
+
 	/**
 	 * sets the inizital state for the class
 	 */
 	public SSGameOfLife(int demention, int demenition2) {
 		this.arr = new String[demention][demenition2];
-		
+		this.nums = new String[demention][demenition2];
+
 		for(int i = 0; i < arr.length; i++) {
 			for(int j = 0; j < arr[i].length; j++) {
 				arr[i][j] = DEAD_CELL;
+				nums[i][j] = DEAD_CELL;
 			}
 		}
 	}
-	
+
 	/**
 	 * set the cell alive
 	 * @param row - the given row
@@ -34,11 +36,43 @@ public class SSGameOfLife {
 			for(int j = 0; j < arr[i].length; j++) {
 				if(i == row && j == col) {
 					arr[i][j] = ALIVE_CELL;
+					nums[i][j] = ALIVE_CELL;
 				}
 			}
 		}
 	}
-	
+
+	/**
+	 * initializes it when doing the game loop
+	 */
+	public void initilaize() {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				if(arr[i][j] == ALIVE_CELL) {
+					nums[i][j] = ALIVE_CELL;
+				}else {
+					nums[i][j] = DEAD_CELL;
+				}
+			}
+		}
+	}
+
+	/**
+	 * get the amount of live cells
+	 * @return - get the total amount of live cells
+	 */
+	public int getAmountAlive(){
+		int count = 0;
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				if(arr[i][j] == ALIVE_CELL) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
 
 	/**
 	 * get the neighbors for the given space
@@ -46,49 +80,94 @@ public class SSGameOfLife {
 	 * @param col - the given col
 	 * @return - the amount of neighbors 0 if none exist
 	 */
-	public int getNeighbors(int row, int col) {
+	public int getNeighbors(int row, int col, String[][] a) {
 		int count = 0;
-		int i = row;
-		int j = col;
-				if(!(i++ < arr.length) && ALIVE_CELL == arr[i+1][j]) {
-					count++;
-				}
-				if((!(i++ < arr.length) && !(j++ < arr[i].length)) &&  ALIVE_CELL == arr[i+1][j+1]) {
-					count++;
-				}
-				if(((i++ < arr.length) && (j-- < 0)) && ALIVE_CELL == arr[i+1][j-1]) {
-					count++;
-				}
-				if(!(i-- < arr.length) &&  ALIVE_CELL == arr[i-1][j]) {
-					count++;
-				}
-				if(((i-- < arr.length) && !(j++ < arr[i].length)) &&  ALIVE_CELL == arr[i-1][j+1]) {
-					count++;
-				}
-				if(((i-- < arr.length) && (j-- < 0)) &&  ALIVE_CELL == arr[i-1][j-1]) {
-					count++;
-				}
-				if(!(j++ < arr[i].length) &&  ALIVE_CELL == arr[i][j+1]) {
-					count++;
-				}
-				if((j-- < 0) &&  ALIVE_CELL == arr[i][j-1]) {
-					count++;
-				}
+		//checks down
+		if(isValid(row+1, col, a)&& ALIVE_CELL == nums[row+1][col]) {
+			count++;
+		}
+		//checks to the down and right
+		if(isValid(row+1, col+1, a) &&  ALIVE_CELL == nums[row+1][col+1]) {
+			count++;
+		}
+		//checks to the down and left
+		if(isValid(row+1, col-1,a) && ALIVE_CELL == nums[row+1][col-1]) {
+			count++;
+		}
+		//checks to the up 
+		if(isValid(row-1, col,a) &&  ALIVE_CELL == nums[row-1][col]) {
+			count++;
+		}
+		//checks to the up and right
+		if(isValid(row-1, col+1,a) &&  ALIVE_CELL == nums[row-1][col+1]) {
+			count++;
+		}
+		//checks to the up and left
+		if(isValid(row-1, col-1,a) &&  ALIVE_CELL == nums[row-1][col-1]) {
+			count++;
+		}
+		//checks to the right
+		if(isValid(row, col+1,a)&&  ALIVE_CELL == nums[row][col+1]) {
+			count++;
+		}
+		//checks to the left
+		if(isValid(row,col-1,a) && ALIVE_CELL == nums[row][col-1]) {
+			count++;
+		}
 		return count;
 	}
-	
-	//just needed to do this so it looks stagesd so i can push
+
 	/**
 	 * return if the cell is alive or not
 	 * @param row - the given row
 	 * @param col - the given col
 	 * @return - true if the cell is alive false otherwise
 	 */
-	public boolean isAlive(int row, int col) {
-		if(arr[row][col].equalsIgnoreCase(ALIVE_CELL)) {
+	public boolean isAlive(int row, int col, String[][] a) {
+		if(a[row][col].equalsIgnoreCase(ALIVE_CELL)) {
 			return true;
 		}else {
 			return false;
+		}
+	}
+
+	/**
+	 * gets the the point and checks if its valid
+	 * @param row -  the given row
+	 * @param col - the given col
+	 * @return - true if the locatoin is valid, false otherwise
+	 */
+	public boolean isValid(int row, int col, String[][] a) {
+		if((row < a.length && row >= 0) && (col < a[0].length && col >= 0)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * plays the game main method uses this in its loop
+	 */
+	public void playGame() {
+		if(SSDriver.day != 1) {
+			initilaize();	
+		}
+		for(int row = 0; row < nums.length; row++) {
+			for(int col = 0; col  < nums[row].length; col++) {
+				if(isAlive(row, col, nums)) {
+					if(getNeighbors(row, col, nums) == 2 || getNeighbors(row, col, nums) == 3) {
+						arr[row][col] = SSGameOfLife.ALIVE_CELL;
+					}else {
+						arr[row][col] = SSGameOfLife.DEAD_CELL;
+					}
+				}else if(!isAlive(row, col, nums)) {
+					if(getNeighbors(row, col, nums) == 3) {
+						arr[row][col] = SSGameOfLife.ALIVE_CELL;
+					}else {
+						arr[row][col] = SSGameOfLife.DEAD_CELL;
+					}
+				}
+			}
 		}
 	}
 
@@ -97,6 +176,6 @@ public class SSGameOfLife {
 	 * @return - the arr
 	 */
 	public String[][] getArr() {
-			return arr;
+		return arr;
 	}
 }
